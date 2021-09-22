e74577ae3103db8e47d8180b646129599a424387
Orhan Albayati <70097003+Orhan92@users.noreply.github.com>
2021-09-22 11:44:45 +0200

    Update SongModel.cs

--- SongModel.cs
+++ SongModel.cs
@@ -7,7 +7,6 @@ namespace CosmosFunction
     {
         [JsonProperty("id")]
         public string Id { get; set; }
-        // [JsonProperty(PropertyName = "artist")]
         public string Artist { get;set; }
         public string Title { get; set; }
         public DateTime Created { get; set; }
