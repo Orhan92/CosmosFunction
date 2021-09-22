3f6ac74ebe1e1e982a349730cdd05d5de5b12d61
Orhan Albayati <70097003+Orhan92@users.noreply.github.com>
2021-09-22 09:43:41 +0200

    Creating Repo

---
+++ SongModel.cs
@@ -0,0 +1,15 @@
+using System;
+using Newtonsoft.Json;
+
+namespace CosmosFunction
+{
+    public class SongModel 
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        // [JsonProperty(PropertyName = "artist")]
+        public string Artist { get;set; }
+        public string Title { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
